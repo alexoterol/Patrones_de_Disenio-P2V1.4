b693d562179b9ba490c2376302c7f2b3cbcfbc21
Alex D Otero <adotero@espol.edu.ec>
2024-07-25 21:26:35 -0500

    Estructura Strategy

---
+++ src/main/java/ec/edu/espol/diseniop2v14/Parrafo2/ContextoTransporte.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.diseniop2v14.Parrafo2;
+
+/**
+ *
+ * @author alexo
+ */
+public class ContextoTransporte {
+    private Strategy strategy;
+    
+    public void setStrategy(Strategy str){
+        
+    }
+    public void doSomething(){
+        
+    }
+    
+}
---
+++ src/main/java/ec/edu/espol/diseniop2v14/Parrafo2/TransporteAereo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.diseniop2v14.Parrafo2;
+
+/**
+ *
+ * @author alexo
+ */
+public class TransporteAereo implements Strategy{
+
+    @Override
+    public void deliver() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void doSomething() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
